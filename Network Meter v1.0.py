1268aad25451a0cd19ef842f07cb39c1842c2786
Soumyajit Dawn <soumyajitdawn@gmail.com>
2021-08-21 18:55:36 +0530

    Revert "removed unneccesary files"
    This reverts commit c5737885dbbe4a6cb36c6d954b2effa3ee513f56.

--- Network Meter v1.0.py
+++ Network Meter v1.0.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from typing import Sized
 import pyspeedtest
 from PIL import ImageTk, Image
 import tkinter.messagebox as tmsg
@@ -49,11 +48,9 @@ navText.pack()
 # city=Entry(fside,textvariable=city).pack()
 
 
-pane = Frame(root,pady=20,borderwidth=2,bg="white")
+pane = Frame(root,pady=50,borderwidth=2,bg="white")
 pane.pack(side=BOTTOM,fill = X)
  
-fb=Frame(pane)
-fb.pack(side=BOTTOM,fill=X) 
 
 pingB= Button(pane, text = "PING",background = "red", fg = "white",font="Calibri 14 bold",command=ping)
 pingB.pack(side = LEFT, expand = True, fill = X)
@@ -64,7 +61,6 @@ downB.pack(side=LEFT, expand = True, fill = X)
  
 upB = Button(pane, text = "UPLOAD SPEED", background = "green", fg = "white",font="Calibri 14 bold", command=up)
 upB.pack(side = LEFT, expand = True, fill = X)
-
 
 
 
